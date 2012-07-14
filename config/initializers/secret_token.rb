7a743476b0e1f05f89d47d84427bdbaa70b3e6c8
Ellen König <ellen@ellenkoenig.de>
2012-07-14 15:23:18 +0200

    Anlegen des Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcards::Application.config.secret_token = '9c6575c0e25a51fe69da6cd7ddefe885d28330b1d543990ea665bf5c727f688f76512c7dbac53048859af68899ba9cf5444738c69c35abb31d69d362dbeb9371'
